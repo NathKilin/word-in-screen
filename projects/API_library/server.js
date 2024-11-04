239c6b5353609bb6b7f7ab679fccbab33f60ea9d
Nathan Kilinski <nathkilin@gmail.com>
2024-11-04 13:16:22 +0200

    use

--- projects/API_library/server.js
+++ projects/API_library/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const bookRoutes = require('./routes/books');
 
 app.get('/', (req, res) => {
     res.send('Servidor está funcionando! Bem-vindo a bordo!');
 });
   
-
 app.listen(PORT, () => {
     console.log(`Servidor está escutando na porta ${PORT}`);
 });
+
+app.use('api', bookRoutes);
