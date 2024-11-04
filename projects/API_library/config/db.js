2195637bfd16c680f5b0f49d89aa049f46926c31
Nathan Kilinski <nathkilin@gmail.com>
2024-11-04 11:14:43 +0200

    jgc

--- projects/API_library/config/db.js
+++ projects/API_library/config/db.js
@@ -7,6 +7,17 @@ const mongoURI = 'mongodb://localhost:27017//library';
 
 // conecting to mongoDB
 mongoose.connect(mongoURI, {
+    // instruction to use the new parsing system 
+    // (the way of interpretate Url)
     userNewUrlParser: true,
+    // configurating the mechanism of conection
     useUnifiedTopology: true,
 })
+.then(()=>{
+    console.log('Conection with MongoDB established with success :)');
+})
+.catch((error) => {
+    console.error('Error to conect with MongoDB: ', error);    
+});
+
+module.exports = mongoose
