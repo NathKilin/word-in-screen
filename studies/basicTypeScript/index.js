2cb68e824808adb54acf835c16f76fcbd6c77d2f
Nathan Kilinski <nathkilin@gmail.com>
2024-12-12 09:17:53 +0200

    testing

---
+++ studies/basicTypeScript/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function calculateArea(length, width) {
+    return length * width;
+}
+const area = calculateArea(5, 3);
+console.log(area);
---
+++ studies/basicTypeScript/index.ts
@@ -0,0 +1,6 @@
+function calculateArea(length: number, width: number): number {
+    return length * width;
+}
+
+const area = calculateArea(5,3);
+console.log(area);
