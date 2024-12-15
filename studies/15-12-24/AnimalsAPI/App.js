a0521ce3f1ec45f2958b395db10628f76976e8ca
Nathan Kilinski <nathkilin@gmail.com>
2024-12-15 11:26:50 +0200

    starting mongooDB

--- studies/15-12-24/AnimalsAPI/App.js
+++ studies/15-12-24/AnimalsAPI/App.js
@@ -1,17 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config()
 const app = express();
 const PORT = 3000;
-const animalsRoutes = require('./animalRoutes.js')
-app.use(express.json())
+const animalsRoutes = require('./animalRoutes.js');
 
+app.use(express.json());
+
+mongoose.connect
 
 app.get('/', (req, res) => {
-    res.send("Hello, there!")
-})
+    res.send("Hello, there!");
+});
 
+app.use('/animals', animalsRoutes);
 
-
-app.listen(PORT,  () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-    
-})
+});
--- studies/15-12-24/AnimalsAPI/animalRoutes.js
+++ studies/15-12-24/AnimalsAPI/animalRoutes.js
@@ -1,6 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/animals', (req, res) => {
-    res.send("This is Animals Route")
-})
+const animals = [
+    { id: 1, name: 'Lion' },
+    { id: 2, name: 'Tiger' },
+    { id: 3, name: 'Elephant' },
+    { id: 4, name: 'Giraffe' },
+    { id: 5, name: 'Zebra' }
+];
+
+// Test route
+router.get('/test', (req, res) => {
+    res.send("we're testing");
+});
+
+// Home route
+router.get('/', (req, res) => {
+    res.send("Animals API Home");
+});
+
+// Random animal route
+router.get('/random', (req, res) => {
+    const randomAnimal = animals[Math.floor(Math.random() * animals.length)];
+    res.send(randomAnimal);
+});
+
+// Get animal by ID
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const animal = animals.find(animal => animal.id == id);
+    if (animal) {
+        res.send(animal);
+    } else {
+        res.status(404).send(`Animal with id: ${id} does not exist`);
+    }
+});
+
+module.exports = router;
